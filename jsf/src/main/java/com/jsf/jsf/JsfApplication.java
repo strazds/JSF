be0ad5857ac2e7e6488c3f8234838e8c8b4ccaa1
Armands Strazds <strazds@gmail.com>
2024-07-29 11:32:38 +0200

    Initial commit

---
+++ jsf/src/main/java/com/jsf/jsf/JsfApplication.java
@@ -0,0 +1,13 @@
+package com.jsf.jsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsfApplication.class, args);
+	}
+
+}
---
+++ jsf/src/test/java/com/jsf/jsf/JsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsf.jsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
